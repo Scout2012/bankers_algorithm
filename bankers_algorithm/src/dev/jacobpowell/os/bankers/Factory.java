fe1449dbf898e9bd018909f542fdb270dfe7ac4d
Jacob Powell <jcbpowell12@gmail.com>
2020-11-15 22:09:12 -0800

    gather resources from file scanner as opposed to input args + closed scanner

--- bankers_algorithm/src/dev/jacobpowell/os/bankers/Factory.java
+++ bankers_algorithm/src/dev/jacobpowell/os/bankers/Factory.java
@@ -6,26 +6,32 @@ import java.util.*;
 public class Factory {
     public static void main(String[] args) {
         String filename = "./src/dev/jacobpowell/os/bankers/infile.txt";
-        int nResources = args.length;
-        int[] resources = new int[nResources];
-        for (int i = 0; i < nResources; i++) { resources[i] = Integer.parseInt(args[i].trim()); }
-        Bank theBank = new BankImpl(resources);
-        int[] maxDemand = new int[nResources];
-        int[] allocated = new int[nResources];
         Thread[] workers = new Thread[Customer.COUNT];      // the customers
         // read in values and initialize the matrices
         // to do
         // ...
         try {
         	File infile = new File(filename);
-        	Scanner readFileContent = new Scanner(infile);
+        	Scanner infileScanner = new Scanner(infile);
         	
+        	String resourceLine = infileScanner.nextLine();
+        	String[] resourcePieces = resourceLine.split(",");
+        	
+        	int nResources = resourcePieces.length;
+            int[] resources = new int[nResources];
+            for (int i = 0; i < nResources; i++) { resources[i] = Integer.parseInt(args[i].trim()); }
+            Bank theBank = new BankImpl(resources);
+            int[] maxDemand = new int[nResources];
+            int[] allocated = new int[nResources];
+            
         	int threadNum = 0;
         	while(threadNum < Customer.COUNT) {
                 workers[threadNum] = new Thread(new Customer(threadNum, maxDemand, theBank));
 				theBank.addCustomer(threadNum, allocated, maxDemand);
                 ++threadNum;        //theBank.getCustomer(threadNum);
             }
+        	
+        	infileScanner.close();
         } catch (FileNotFoundException fnfe) { throw new Error("Unable to find file \"" + filename + "\"");
         } catch (IOException ioe) { throw new Error("Error processing \"" + filename + "\""); }
         System.out.println("FACTORY: created threads");     // start the customers
